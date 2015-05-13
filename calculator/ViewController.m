938607083b8ee1740177e05e14ca0ae97dd74e7a
Your Name <everettchurchey@crbc.edu>
2015-05-13 10:17:44 -0400

    calculator

--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -9,11 +9,26 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textBox1;
+
+@property (weak, nonatomic) IBOutlet UITextField *textBox2;
+@property (weak, nonatomic) IBOutlet UILabel *LBLanswer;
 
 @end
 
 @implementation ViewController
 
+
+- (IBAction)add:(id)sender {
+    NSString* baseNumber = [[self textBox1] text];
+    NSInteger a = [baseNumber integerValue];
+    NSString* addNumber = [[self textBox2] text];
+    NSInteger b = [addNumber integerValue];
+    NSInteger answer = a + b;
+    NSString *inStr = [NSString stringWithFormat: @"%ld", answer];
+    [[self LBLanswer] setText: inStr];
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
